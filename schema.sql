bc4d6846d53e3f48f5575913ea1a2079b8a9efcb
Jordan <jordan.b3360@gmail.com>
2022-09-07 21:15:29 -0500

    added comments to schema and seeds SQL files

--- schema.sql
+++ schema.sql
@@ -1,14 +1,21 @@
+-- Drop database if already exists
 DROP DATABASE IF EXISTS employee_db;
+-- Creates database
 CREATE DATABASE employee_db;
 
+-- Selects database to use
 USE employee_db;
 
+-- Creates department table
 CREATE TABLE department (
+  -- Creates id and name
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
+-- Creates role table
 CREATE TABLE role (
+  -- Creates id, title, salary, department_id with foreign key and reference
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title TEXT NOT NULL,
   salary DECIMAL NOT NULL,
@@ -18,7 +25,9 @@ CREATE TABLE role (
   ON DELETE SET NULL
 );
 
+-- Creates employee table
 CREATE TABLE employee (
+   -- Creates id, title, salary, department_id with foreign key and reference
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
--- seeds.sql
+++ seeds.sql
@@ -1,15 +1,17 @@
+-- Inserts values into department table
 INSERT INTO department (id, name)
 VALUES (1, "Engineering"),
        (2, "Management"),
        (3, "HR"),
        (4, "IT");
-
+-- Inserts values into role table
 INSERT INTO role (id, title, salary, department_id)
 VALUES (1, "Engineer", 60000, 1),
        (2,"Manager", 70000, 2),
        (3,"Human Resources", 50000, 3),
        (4,"Help Desk", 40000, 4);
 
+-- Inserts values into employee table
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (1, "Bob", "Jones", 2, null),
        (2,"John", "Deere", 1, 1),
